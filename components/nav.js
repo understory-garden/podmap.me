d075524952f28c9180d1f1573e3ae4b58c32a4f5
Travis Vachon <travis.vachon@gmail.com>
2020-09-27 20:08:37 -0700

    tweak logo

--- components/nav.js
+++ components/nav.js
@@ -8,21 +8,12 @@ const links = [
 export default function Nav() {
   return (
     <nav>
-      <ul className="flex justify-between items-center p-8">
+      <ul className="flex justify-between items-center p-3 mx-3">
         <li>
           <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
+            <a className="text-2xl no-underline">podmap.me</a>
           </Link>
         </li>
-        <ul className="flex justify-between items-center space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
-                {label}
-              </a>
-            </li>
-          ))}
-        </ul>
       </ul>
     </nav>
   )
