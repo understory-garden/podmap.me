ce5d4b70594c5ef46acbc1a02d27253bb38d54eb
Travis Vachon <travis.vachon@gmail.com>
2020-10-10 15:40:45 -0700

    update dependencies, README, add license

--- pages/credits.jsx
+++ pages/credits.jsx
@@ -6,9 +6,11 @@ export default function CreditsPage(){
       <Nav/>
       <main className="text-center text-lg w-128 m-auto ">
         <p className="my-6">
-          podmap.me is a web-based implementation of the
-          &nbsp;<a href="https://batjc.wordpress.com/">Bay Area Transformative Justice Collective's</a>&nbsp;
-          <a href="https://batjc.wordpress.com/pods-and-pod-mapping-worksheet/">Pod Mapping Worksheet</a>.
+          podmap.me is a Web-based implementation of
+          the <a href="https://batjc.wordpress.com/pods-and-pod-mapping-worksheet/">Pod Mapping Worksheet</a> created
+          by <a href="https://www.instagram.com/mia.mingus/">Mia Mingus</a> for
+          the <a href="https://batjc.wordpress.com/">Bay Area Transformative Justice Collective</a>.
+          .
         </p>
         <p>
           if you find it helpful please <a href="https://batjc.wordpress.com/donate/">donate</a>.
