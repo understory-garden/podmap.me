2d610dabb4aba07ece57993b15564866a1767c64
Travis Vachon <travis.vachon@gmail.com>
2020-10-14 17:15:51 -0700

    remove unused links data

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,10 +1,5 @@
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
-
 export default function Nav() {
   return (
     <nav>
