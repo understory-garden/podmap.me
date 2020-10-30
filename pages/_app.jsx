02198578bf50df59eec8fdf553ff80753bea1c79
Travis Vachon <travis.vachon@gmail.com>
2020-10-30 09:24:36 -0700

    monetize podmap.me in a special wallet for donation to BATJC

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,22 @@
+import Head from 'next/head'
+
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="relative bg-white overflow-hidden">
+      <Head>
+        <title>map your pods!</title>
+        {/* XRP pointer from my uphold account, special wallet for BATJC */}
+        <meta name="monetization" content="$ilp.uphold.com/JUjA2yEE6LRA" />
+      </Head>
+      <main className="mx-auto max-w-screen-xl">
+        <Component {...pageProps} />
+      </main>
+      <footer className="text-center my-12 text-xs text-gray-600">
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
